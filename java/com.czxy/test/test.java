549957579ad8199519f47ec03d1ad99b74e6f422
wcl0918 <wcl0918@outlook.com>
2020-09-18 08:45:06 +0800

    第一次提交

---
+++ java/com.czxy/test/test.java
@@ -0,0 +1,13 @@
+package com.czxy.test;
+
+/**
+ * @Author wcl09
+ * @Version v 1.0
+ * @Date 2020/9/18
+ */
+public class test {
+
+    public static void main(String[] args) {
+        System.out.println("12312313");
+    }
+}
